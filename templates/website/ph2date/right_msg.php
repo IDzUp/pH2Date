7039bf72e6027c0145e034a0a445cc650e89524f
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-11 00:24:53 +0100

    Update condition if() msg block

--- templates/website/ph2date/right_msg.php
+++ templates/website/ph2date/right_msg.php
@@ -1,7 +1,7 @@
-<?php if(!empty($this->msg_error_right) ): ?>
-    <div id="" class="alert alert-danger"><?=$this->msg_error_right?></div>
+<?php if (!empty($this->msg_error_right)): ?>
+    <div id="" class="alert alert-danger"><?= $this->msg_error_right ?></div>
 <?php endif ?>
 
-<?php if(!empty($this->msg_success_right)): ?>
-    <div class="alert alert-success"><?=$this->msg_success_right?></div>
+<?php if (!empty($this->msg_success_right)): ?>
+    <div class="alert alert-success"><?= $this->msg_success_right ?></div>
 <?php endif ?>

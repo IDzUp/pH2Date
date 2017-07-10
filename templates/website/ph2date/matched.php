737e9c22de203e5906f58822bfe99ba25637ef1d
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-11 00:23:34 +0100

    Template indentation fix

--- templates/website/ph2date/matched.php
+++ templates/website/ph2date/matched.php
@@ -4,59 +4,63 @@ $this->load->view('loged_in_side');
 ?>
 
 <div class="col-md-8">
-<div class="well">
+    <div class="well">
 
-<?php if(!empty($this->msg_error_right) ): ?>
-    <div id="" class="alert alert-danger"><?php echo $this->msg_error_right;?></div>
-<?php endif ?>
+        <?php if (!empty($this->msg_error_right)): ?>
+            <div id="" class="alert alert-danger"><?php echo $this->msg_error_right; ?></div>
+        <?php endif ?>
 
-<?php if(!empty($this->msg_success_right)): ?>
-    <div class="alert alert-success"><?php echo $this->msg_success_right;?></div>
-<?php endif ?>
+        <?php if (!empty($this->msg_success_right)): ?>
+            <div class="alert alert-success"><?php echo $this->msg_success_right; ?></div>
+        <?php endif ?>
 
-<h1><?=lang('matched_users')?></h1>
-<div class="clr"></div>
-</br>
+        <h1><?= lang('matched_users') ?></h1>
+        <div class="clr"></div>
+        </br>
 
-<?php if(!empty($users)): ?>
-    <?php foreach($users AS $user): ?>
+        <?php if (!empty($users)): ?>
+            <?php foreach ($users AS $user): ?>
 
-<div class="row" style="border-bottom: 1px solid #ccc;margin-top:10px;">
-<?php $profile_photo = ($user->profile_photo)?$user->profile_photo:'default_img.png';?>
-<div class="col-md-4">
-<img class="pic img-circle" src="<?php echo base_url('assets/uploads/member/'.$profile_photo);?>" width="150" height="150" alt="Profile Picture" />
+                <div class="row" style="border-bottom: 1px solid #ccc;margin-top:10px;">
+                    <?php $profile_photo = ($user->profile_photo) ? $user->profile_photo : 'default_img.png'; ?>
+                    <div class="col-md-4">
+                        <img class="pic img-circle"
+                             src="<?php echo base_url('assets/uploads/member/' . $profile_photo); ?>" width="150"
+                             height="150" alt="Profile Picture"/>
 
-<a style="margin-left:20px;" href="<?php echo site_url('user/profile/'.strip_tags($user->username)) ?>" class="btn btn-danger" ><?=lang('visit_profile')?></a>
+                        <a style="margin-left:20px;"
+                           href="<?php echo site_url('user/profile/' . strip_tags($user->username)) ?>"
+                           class="btn btn-danger"><?= lang('visit_profile') ?></a>
 
-</div>
+                    </div>
 
-<div class="col-md-8">
+                    <div class="col-md-8">
 
-<table class="table table-striped">
-<tr>
-  <td class=""><?=lang('user_name')?></td>
-  <td class=""><?php echo strip_tags(ucfirst($user->first_name) .' '. ucfirst($user->last_name)) ?></td>
-</tr>
-<tr>
-  <td class=""><?=lang('user_sex')?></td>
-  <td class=""><?php echo ucfirst(strip_tags($user->sex)) ?></td>
-</tr>
-<tr>
-  <td class=""><?=lang('user_country')?></td>
-  <td class=""><?php echo strip_tags($user->country) ?></td>
-</tr>
-<tr>
-  <td class=""><?=lang('user_city')?></td>
-  <td class=""><?php echo ucfirst(strip_tags($user->city)) ?></td>
-</tr>
+                        <table class="table table-striped">
+                            <tr>
+                                <td class=""><?= lang('user_name') ?></td>
+                                <td class=""><?php echo strip_tags(ucfirst($user->first_name) . ' ' . ucfirst($user->last_name)) ?></td>
+                            </tr>
+                            <tr>
+                                <td class=""><?= lang('user_sex') ?></td>
+                                <td class=""><?php echo ucfirst(strip_tags($user->sex)) ?></td>
+                            </tr>
+                            <tr>
+                                <td class=""><?= lang('user_country') ?></td>
+                                <td class=""><?php echo strip_tags($user->country) ?></td>
+                            </tr>
+                            <tr>
+                                <td class=""><?= lang('user_city') ?></td>
+                                <td class=""><?php echo ucfirst(strip_tags($user->city)) ?></td>
+                            </tr>
 
-</table>
+                        </table>
 
-</div>
-</div>
-    <?php endforeach ?>
-<?php endif ?>
+                    </div>
+                </div>
+            <?php endforeach ?>
+        <?php endif ?>
 
-</div>
+    </div>
 </div>
 <?php $this->load->view('footer') ?>
--- templates/website/ph2date/message_box.php
+++ templates/website/ph2date/message_box.php
@@ -2,33 +2,36 @@
 $this->load->view('header');
 $this->load->view('loged_in_side');
 ?>
-<div class="col-md-8">
-<div class="well">
+    <div class="col-md-8">
+        <div class="well">
 
-<?php if(!empty($this->msg_error_right) ): ?>
-    <div id="" class="alert alert-danger"><?php echo $this->msg_error_right;?></div>
-<?php endif ?>
+            <?php if (!empty($this->msg_error_right)): ?>
+                <div id="" class="alert alert-danger"><?php echo $this->msg_error_right; ?></div>
+            <?php endif ?>
 
-<?php if(!empty($this->msg_success_right)): ?>
-    <div class="alert alert-success"><?php echo $this->msg_success_right;?></div>
-<?php endif ?>
+            <?php if (!empty($this->msg_success_right)): ?>
+                <div class="alert alert-success"><?php echo $this->msg_success_right; ?></div>
+            <?php endif ?>
 
-<h1><?=lang('my_messages')?></h1>
-<div class="clr"></div>
-</br>
+            <h1><?= lang('my_messages') ?></h1>
+            <div class="clr"></div>
+            </br>
 
-  <?php if(!empty($messages)): ?>
-      <?php foreach ($messages as $message): ?>
-          <p><b><?=strip_tags(ucfirst($message->from).' '.$message->sent)?>: </b><?=$message->message?></p>
-      <?php endforeach ?>
-  <?php endif ?>
+            <?php if (!empty($messages)): ?>
+                <?php foreach ($messages as $message): ?>
+                    <p><b><?= strip_tags(ucfirst($message->from) . ' ' . $message->sent) ?>
+                            : </b><?= $message->message ?></p>
+                <?php endforeach ?>
+            <?php endif ?>
 
-      <form action="<?php echo site_url('user/message_box/'.strip_tags($other_user->username)) ?>"  method="post">
-<div class="col-md-10">
-<textarea class="form-control"  id="message" name="message"></textarea>
-</div>
-      <button type="submit" class="btn btn-default" style="line-height:40px;margin-left:15px;margin-top:2px;"><?=lang('edit_user_submit_btn')?></button>
+            <form action="<?php echo site_url('user/message_box/' . strip_tags($other_user->username)) ?>"
+                  method="post">
+                <div class="col-md-10">
+                    <textarea class="form-control" id="message" name="message"></textarea>
+                </div>
+                <button type="submit" class="btn btn-default"
+                        style="line-height:40px;margin-left:15px;margin-top:2px;"><?= lang('edit_user_submit_btn') ?></button>
 
-  </div>
-</div>
+        </div>
+    </div>
 <?php $this->load->view('footer') ?>

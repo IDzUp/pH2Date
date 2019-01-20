5a8fa89e4c1b7d08e40357a62f1284672c2a20c6
Pierre-Henry Soria <hi@ph7.me>
2019-01-21 11:29:00 +1300

    [Env Mode] Send 503 HTTP status by default if other modes than mentioned

--- index.php
+++ index.php
@@ -43,6 +43,7 @@ if (defined('ENVIRONMENT'))
         break;
 
         default:
+            header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
             exit('The application environment is not set correctly.');
     }
 }

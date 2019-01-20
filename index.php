972376b192110270fe22f45a5b07d5c2b085c1b1
Pierre-Henry Soria <hi@ph7.me>
2019-01-21 11:25:34 +1300

    Remove "testing" option for the available environments

--- index.php
+++ index.php
@@ -37,7 +37,6 @@ if (defined('ENVIRONMENT'))
             ini_set('display_errors', 1);
         break;
 
-        case 'testing':
         case 'production':
             error_reporting(0);
             ini_set('display_errors', 0);

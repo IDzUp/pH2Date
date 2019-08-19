c82561cb515bf8c80cc55885c9a7c67b35d2186a
Pierre-Henry Soria <hi@ph7.me>
2019-08-19 13:33:50 +0200

    Consistency: Swapped double quote to single one + Better variable name

--- application/controllers/cron.php
+++ application/controllers/cron.php
@@ -15,24 +15,24 @@ class cron extends Website_Controller
         define('FFMPEG_movie', '/usr/bin/ffmpeg_movie');
         define('flvtool2Path', '/usr/bin/flvtool2');
 
-        $videoDirName = FCPATH . "assets/uploads/files/video/video/";  //Folder name where video will save
-        $videoThumDir = FCPATH . "assets/uploads/files/video/thum/";
+        $videoDirName = FCPATH . 'assets/uploads/files/video/video/';  //Folder name where video will save
+        $videoThumpDir = FCPATH . 'assets/uploads/files/video/thum/';
 
         $this->db->select('id, video_file');
-        $videos = $this->db->get_where('video', array('converted' => 1))->result();
+        $videos = $this->db->get_where('video', ['converted' => 1])->result();
 
         foreach ($videos as $video) {
             $fileNameNoExtension = preg_replace("/\.[^.]+$/", "", $video->video_file);
             $video_path = FCPATH . "assets/uploads/video/$video->video_file";
 
-            $randomString = $video->id . "_" . substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 10);
+            $randomString = $video->id . '_' . substr(str_shuffle('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'), 0, 10);
 
             $destinationVideo = $videoDirName . "{$randomString}.webm";
-            $destinationThum = $videoThumDir . "{$randomString}.jpg";
+            $destinationThum = $videoThumpDir . "{$randomString}.jpg";
             shell_exec("ffmpeg -i $video_path -vcodec libvpx -acodec libvorbis $destinationVideo 2>&1");
             shell_exec("ffmpeg -i $destinationVideo -f mjpeg -t 0.050 $destinationThum 2>&1");
-            $data["converted"] = 1;
-            $data["converted_name"] = $randomString;
+            $data['converted'] = 1;
+            $data['converted_name'] = $randomString;
             $this->db->update('video', $data, "id = $video->id");
         }
     }
